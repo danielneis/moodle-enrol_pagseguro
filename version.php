5d9fd2e70b6ab10a02771e0bf8b9aa0f445ba912
Daniel Neis Araujo <danielneis@gmail.com>
2020-04-22 16:18:09 -0300

    bump version 18

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020021000;
+$plugin->version   = 2020042200;
 $plugin->requires  = 2018120300;
-$plugin->release   = '17';
+$plugin->release   = '18';
 $plugin->component = 'enrol_pagseguro';
 $plugin->maturity  = MATURITY_STABLE;
